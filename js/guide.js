e4a10f329b25534d1e8e7cc5e2b11b40f804e3ed
lukaprotrka <luka.protrka@gmail.com>
2026-04-08 20:34:40 +0200

    got rid of bug with {}

--- js/guide.js
+++ js/guide.js
@@ -13,7 +13,7 @@ const tireData = [
     {
         name: 'Soft',
         color: '#e10600',
-        image: '{css,js,data,images}/images/tires/soft.svg',
+        image: 'images/tires/soft.svg',
         description: 'Fastest but least durable',
         durability: 'Low',
         usage: 'Qualifying, short stints',
@@ -22,7 +22,7 @@ const tireData = [
     {
         name: 'Medium',
         color: '#ffb800',
-        image: '{css,js,data,images}/images/tires/medium.svg',
+        image: 'images/tires/medium.svg',
         description: 'Balanced performance',
         durability: 'Medium',
         usage: 'Race strategy balance',
@@ -31,7 +31,7 @@ const tireData = [
     {
         name: 'Hard',
         color: '#ffffff',
-        image: '{css,js,data,images}/images/tires/hard.svg',
+        image: 'images/tires/hard.svg',
         description: 'Most durable compound',
         durability: 'High',
         usage: 'Long race stints',
@@ -40,7 +40,7 @@ const tireData = [
     {
         name: 'Intermediate',
         color: '#00d084',
-        image: '{css,js,data,images}/images/tires/intermediate.svg',
+        image: 'images/tires/intermediate.svg',
         description: 'For light rain/drying track',
         durability: 'Medium',
         usage: 'Damp conditions',
@@ -49,7 +49,7 @@ const tireData = [
     {
         name: 'Wet',
         color: '#0090ff',
-        image: '{css,js,data,images}/images/tires/full_wet.svg',
+        image: 'images/tires/full_wet.svg',
         description: 'For heavy rain',
         durability: 'High',
         usage: 'Heavy wet conditions',
@@ -61,67 +61,67 @@ const tireData = [
 // Flag data
 const flagData = [
     {
-        image: '{css,js,data,images}/images/flags/checkered.svg',
+        image: 'images/flags/checkered.svg',
         name: 'Chequered Flag',
         color: 'Black & White',
         description: 'Race finished - shown to race winner and all cars crossing the line after.'
     },
     {
-        image: '{css,js,data,images}/images/flags/red.svg',
+        image: 'images/flags/red.svg',
         name: 'Red Flag',
         color: 'Red',
         description: 'Session stopped - drivers must return to pit lane immediately due to dangerous conditions.'
     },
     {
-        image: '{css,js,data,images}/images/flags/yellow.svg',
+        image: 'images/flags/yellow.svg',
         name: 'Yellow Flag',
         color: 'Yellow',
         description: 'Danger ahead - no overtaking, reduce speed. Can be single or double waved for severity.'
     },
     {
-        image: '{css,js,data,images}/images/flags/green.svg',
+        image: 'images/flags/green.svg',
         name: 'Green Flag',
         color: 'Green',
         description: 'Track is clear - racing can resume, often shown after yellow flag zones.'
     },
     {
-        image: '{css,js,data,images}/images/flags/blue.svg',
+        image: 'images/flags/blue.svg',
         name: 'Blue Flag',
         color: 'Blue',
         description: 'Faster car approaching - driver must let faster car pass within three flags shown.'
     },
     {
-        image: '{css,js,data,images}/images/flags/black.svg',
+        image: 'images/flags/black.svg',
         name: 'Black Flag',
         color: 'Black',
         description: 'Disqualification - driver must return to pits immediately, shown with car number.'
     },
     {
-        image: '{css,js,data,images}/images/flags/white.svg',
+        image: 'images/flags/white.svg',
         name: 'White Flag',
         color: 'White',
         description: 'Slow driver ahead.'
     },
     {
-        image: '{css,js,data,images}/images/flags/black_white.svg',
+        image: 'images/flags/black_white.svg',
         name: 'Black & White Flag',
         color: 'Diagonal',
         description: 'Warning for unsportsmanlike behavior - final warning before penalties.'
     },
     {
-        image: '{css,js,data,images}/images/flags/black_orange.svg',
+        image: 'images/flags/black_orange.svg',
         name: 'Black/Orange Flag',
         color: 'Black + Orange',
         description: 'Mechanical problem - car has damage and must pit for repairs.'
     },
     {
-        image: '{css,js,data,images}/images/flags/yellow_red_stripes.svg',
+        image: 'images/flags/yellow_red_stripes.svg',
         name: 'Yellow Flag with Red Stripes',
         color: 'Yellow + Red',
         description: 'Shown to inform drivers that there has been a deterioration in grip levels on the track ahead'
     },
     {
-        image: '{css,js,data,images}/images/flags/code60.svg',
+        image: 'images/flags/code60.svg',
         name: 'Code 60',
         color: 'Pink',
         description: 'Slow down to 60 km/h (37 mph). No overtaking.'
